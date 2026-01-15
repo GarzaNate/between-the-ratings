80f67a5c3916534f7e7952d81fd0c20435bf91b8
slendyG <nategarza2410@gmail.com>
2026-01-14 23:05:25 -0800

    Update global styles and enhance Home component with background color

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   console.log("Home page loaded");
   return (
-    <main>
+    <main className="bg-primary">
       <div className="text-5xl underline">Welcome to Between The Ratings!</div>
     </main>
   );
