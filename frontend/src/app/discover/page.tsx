b26806dd83495ff586ccf6a4e0e9ecc0a140ea51
slendyG <nategarza2410@gmail.com>
2026-07-20 10:28:07 -0700

    feat: create Discover component with initial structure for API testing

---
+++ frontend/src/app/discover/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import page from '../page'
+
+// Need to create a search bar to test if API is working.
+
+const Discover = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default Discover
